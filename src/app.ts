d208d3085081f5150bc673f4450c0af76a618f55
Thomas <thomas.wellan@gmail.com>
2019-04-27 15:11:39 +0200

    Update ts config to allow js lib + add jump and better control

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 /// <reference path="../node_modules/phaser3-docs/typescript/phaser.d.ts" />
-
 import "phaser";
 import MainScene from "./scripts/scenes/MainScene";
 import "./styles/style.scss";
+import PhaserMatterCollisionPlugin from "phaser-matter-collision-plugin";
 
 const gameConfig: GameConfig = {
   backgroundColor: "#000000",
@@ -11,11 +11,20 @@ const gameConfig: GameConfig = {
   physics: {
     default: "matter",
     matter: {
-      debug: false, // true for collisions debug
+      debug: true, // true for collisions debug
       gravity: { y: 1 } // This is the default value, so we could omit this
     },
   },
   scene: [MainScene],
+  plugins: {
+    scene: [
+      {
+        plugin: PhaserMatterCollisionPlugin, // The plugin class
+        key: "matterCollision", // Where to store in Scene.Systems, e.g. scene.sys.matterCollision
+        mapping: "matterCollision" // Where to store in the Scene, e.g. scene.matterCollision
+      }
+    ]
+  },
   type: Phaser.AUTO,
   width: document.body.offsetWidth,
 };
