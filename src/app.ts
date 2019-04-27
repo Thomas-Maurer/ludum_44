1e1998dd9914eee2144660953c7e124e5c0b90c5
Thomas <thomas.wellan@gmail.com>
2019-04-27 07:42:12 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+/// <reference path="../node_modules/phaser3-docs/typescript/phaser.d.ts" />
+
+import "phaser";
+import MainScene from "./scripts/scenes/MainScene";
+import "./styles/style.scss";
+
+const gameConfig: GameConfig = {
+  backgroundColor: "#000000",
+  height: window.innerHeight,
+  parent: "game",
+  physics: {
+    arcade: {
+      debug: false, // true for collisions debug
+    },
+    default: "arcade",
+  },
+  scene: [MainScene],
+  type: Phaser.AUTO,
+  width: document.body.offsetWidth,
+};
+const game = new Phaser.Game(gameConfig);
---
+++ src/scripts/scenes/MainScene.ts
@@ -0,0 +1,15 @@
+export default class MainScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: "MainScene",
+    });
+  }
+  preload() {
+    this.load.image("gameImage", "assets/graphics/ui/game.png");
+  }
+  create() {
+    this.add.image(400, 300, "gameImage");
+  }
+
+}
+
