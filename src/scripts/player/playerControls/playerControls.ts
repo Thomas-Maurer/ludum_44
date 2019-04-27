68ffc94c7d2632ae8a3ade2577b03e5489b8a7c3
Thomas <thomas.wellan@gmail.com>
2019-04-27 16:41:16 +0200

    tweek control

--- src/scripts/player/playerControls/playerControls.ts
+++ src/scripts/player/playerControls/playerControls.ts
@@ -23,20 +23,22 @@ export class PlayerControls {
         let negativeforceVector: Vector2;
         let forceVector: Vector2;
         let body: any = player.getPlayerSprite().body;
-        if (player.canPlayerAct()) {
-            forceVector = new Vector2(-0.1);
-            negativeforceVector = new Vector2(0.1);
-            if (body.velocity.x > 0.3) {
+        if (!player.canPlayerAct()) {
+            // Player is in the Air
+            forceVector = new Vector2(0.1);
+            negativeforceVector = new Vector2(-0.1);
+            if (body.velocity.x >= 0.3) {
                 player.getPlayerSprite().setVelocity(0.3);
-            } else if (body.velocity.x < -0.3) {
+            } else if (body.velocity.x <= -0.3) {
                 player.getPlayerSprite().setVelocity(-0.3);
             }
         } else {
-            negativeforceVector = new Vector2(-0.1,0);
-            forceVector = new Vector2(0.5,0);
-            if (body.velocity.x > 0.1) {
+            //Player touch the ground
+            negativeforceVector = new Vector2(-0.1);
+            forceVector = new Vector2(0.5);
+            if (body.velocity.x >= 0.1) {
                 player.getPlayerSprite().setVelocity(0.1);
-            } else if (body.velocity.x < -0.15) {
+            } else if (body.velocity.x <= -0.15) {
                 player.getPlayerSprite().setVelocity(-0.15);
             }
         }
