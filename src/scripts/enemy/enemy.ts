15622dcc4eef5b2015d0355fbab04549de667e6e
Domingues Raphael <tmp@jacta.fr>
2019-04-29 23:41:55 +0200

    Revert "Merge branch 'priest-bullet'"
    This reverts commit 66dd256dd0d447b86464e8eae1f5aa60364a2bfd.

--- src/scripts/enemy/enemy.ts
+++ src/scripts/enemy/enemy.ts
@@ -149,6 +149,7 @@ export class Enemy extends Phaser.Physics.Matter.Sprite implements IEnemy {
         this.setExistingBody(compoundBody);
         this.setFixedRotation();
         this.setFriction(0);
+        this.setOrigin(0.5, 0.65);
     }
 
     /**
@@ -268,6 +269,8 @@ export class Enemy extends Phaser.Physics.Matter.Sprite implements IEnemy {
         this.setStatic(true);
         this.x = currentX;
         this.y = currentY;
+        this.setOrigin(0.5, 0.65);
+
     }
 
     /**
