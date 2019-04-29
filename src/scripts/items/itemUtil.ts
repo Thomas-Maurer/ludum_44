0fdf1d7d5dd1eee7bf42f47f3dd7e0ec4083f373
Thomas <thomas.wellan@gmail.com>
2019-04-30 01:52:30 +0200

    Placement

--- src/scripts/items/itemUtil.ts
+++ src/scripts/items/itemUtil.ts
@@ -12,15 +12,19 @@ export default class ItemUtil {
      */
     public generateItem(obj: any): void {
         let frameName = '';
+        let cost = 15;
         if (obj.name === 'glasses') {
             frameName = 'items/iconglasses.png';
+            cost = 10;
         } else if (obj.name === 'umbrella') {
+            cost = 20;
             frameName = 'items/umbrella1.png';
         } else if ((obj.name === 'pipe')){
+            cost = 5;
             frameName = 'items/pipe.png'
         }
         let boostItem = new BoostItem(this.scene.matter.world, this.scene, obj.x, obj.y, 'all_sprites', frameName);
-        boostItem.setHpCost(15);
+        boostItem.setHpCost(cost);
         boostItem.setItemName(obj.name);
         boostItem.setCollisionCategory(this.scene.itemsCat);
         boostItem.setCollidesWith([this.scene.playerCatCollision, 1, this.scene.itemsCat]);
