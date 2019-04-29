f2fb9d4fbd9b72388e7815323b0bf72c3d1c284e
Thomas <thomas.wellan@gmail.com>
2019-04-29 16:21:00 +0200

    Enable dash

--- src/scripts/items/itemUtil.ts
+++ src/scripts/items/itemUtil.ts
@@ -11,14 +11,15 @@ export default class ItemUtil {
      * generate random item on spawn
      */
     public generateItem(obj: any): void {
-        let itemSprite = new BoostItem(this.scene.matter.world, this.scene, obj.x, obj.y, 'all_sprites', 'items/dashpotion1.png');
+        let boostItem = new BoostItem(this.scene.matter.world, this.scene, obj.x, obj.y, 'all_sprites', 'items/dashpotion1.png');
         const itemAnim = this.scene.player.generateFrameNames('items/dashpotion', 'all_sprites', 1, 4);
         this.scene.anims.create({ key: 'dashpotionAnim', frames: itemAnim, frameRate: 5, repeat: -1 });
-        itemSprite.play('dashpotionAnim');
-        itemSprite.setDensity(50);
-        itemSprite.setHpCost(10);
+        boostItem.play('dashpotionAnim');
+        boostItem.setDensity(50);
+        boostItem.setHpCost(10);
+        boostItem.setItemName('dashPotion');
         // itemSprite.setStatic(true);
-        itemSprite.setCollisionCategory(this.scene.itemsCat);
-        itemSprite.setCollidesWith([this.scene.playerCatCollision, 1, this.scene.itemsCat]);
+        boostItem.setCollisionCategory(this.scene.itemsCat);
+        boostItem.setCollidesWith([this.scene.playerCatCollision, 1, this.scene.itemsCat]);
     }
 }
